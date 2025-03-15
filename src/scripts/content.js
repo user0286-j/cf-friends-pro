bdf97c33c8a9e765df285e5952ba0ea9bbf3ff06
unknown <jhamsidrbz@gmail.com>
2025-03-15 12:47:59 -0500

    I added a table for show names

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,5 +1,26 @@
 "use strict";
-var notificar = function (nombre) {
-    console.log(nombre);
-};
-notificar("hola");
+var table = document.querySelector("#pageContent > div.datatable > div:nth-child(6) > table");
+if (table) {
+    var names = [];
+    var fila = table.rows;
+    for (var i = 1; i < fila.length; i++) {
+        console.log(fila[i].innerText.split('\t')[1]);
+        names.push(fila[i].innerText.split('\t')[1]);
+    }
+    var contenido = document.querySelector("#pageContent");
+    if (contenido) {
+        var tabla_1 = document.createElement("table");
+        names.forEach(function (name, index) {
+            var tr = document.createElement("tr");
+            tr.innerHTML = "<td class=\"dark left\">".concat(index, "</td>\n<td style=\"text-align:left;\" class=\"dark\">").concat(name, "</td>\n<td class=\"dark right\">last</td>");
+            tabla_1.appendChild(tr);
+        });
+        contenido.appendChild(tabla_1);
+    }
+    else {
+        alert("la extensión no se puede cargar :(");
+    }
+}
+else {
+    console.log("tabla no encontrada");
+}
--- typescript/content.ts
+++ typescript/content.ts
@@ -1,6 +1,29 @@
-const notificar: any = (nombre: string) => {
-    console.log(nombre);
-};
+var table = document.querySelector<HTMLTableElement>("#pageContent > div.datatable > div:nth-child(6) > table");
 
+if (table){
 
-notificar("hola");
+    const names: string[] = [];
+    const fila = table.rows;
+    for (let i: number = 1; i < fila.length; i++){
+        //console.log(fila[i].innerText.split('\t')[1]);
+        names.push(fila[i].innerText.split('\t')[1]);
+    }
+
+    const contenido = document.querySelector<HTMLDivElement>("#pageContent");
+    if (contenido){
+        
+        const tabla = document.createElement("table");
+        names.forEach((name: string, index: number) => {
+            const tr = document.createElement("tr");
+            tr.innerHTML = `<td class="dark left">${index}</td>\n<td style="text-align:left;" class="dark">${name}</td>\n<td class="dark right">last</td>`;
+            tabla.appendChild(tr);
+        });
+        contenido.appendChild(tabla);
+    }else{
+        alert("la extensión no se puede cargar :(");
+    }
+
+ 
+}else{
+    console.log("tabla no encontrada");
+}
