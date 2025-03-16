d75016347ba32ed8d31b011f08d4e5009b40e27b
unknown <jhamsidrbz@gmail.com>
2025-03-16 16:06:46 -0500

    added getdatauser

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,21 +1,44 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var table = document.querySelector("#pageContent > div.datatable > div:nth-child(6) > table");
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function get_last_contest(handle, ms) {
+    return __awaiter(this, void 0, void 0, function* () {
+        delay(ms);
+        let url = `https://codeforces.com/api/user.rating?handle=${handle}`;
+        const response = yield fetch(url);
+        const data = yield response.json();
+        console.log(handle, data.status);
+        return data;
+    });
+}
 if (table) {
-    var names = [];
-    var fila = table.rows;
-    for (var i = 1; i < fila.length; i++) {
-        console.log(fila[i].innerText.split('\t')[1]);
+    const names = [];
+    const fila = table.rows;
+    for (let i = 1; i < fila.length; i++) {
+        //console.log(fila[i].innerText.split('\t')[1]);
         names.push(fila[i].innerText.split('\t')[1]);
     }
-    var contenido = document.querySelector("#pageContent");
+    const contenido = document.querySelector("#pageContent");
     if (contenido) {
-        var tabla_1 = document.createElement("table");
-        names.forEach(function (name, index) {
-            var tr = document.createElement("tr");
-            tr.innerHTML = "<td class=\"dark left\">".concat(index, "</td>\n<td style=\"text-align:left;\" class=\"dark\">").concat(name, "</td>\n<td class=\"dark right\">last</td>");
-            tabla_1.appendChild(tr);
+        const tabla = document.createElement("table");
+        names.forEach((name, index) => {
+            const tr = document.createElement("tr");
+            tr.innerHTML = `<td class="dark left">${index}</td>\n<td style="text-align:left;" class="dark">${name}</td>\n<td class="dark right">last</td>`;
+            tabla.appendChild(tr);
+            get_last_contest(name, index * 1000);
         });
-        contenido.appendChild(tabla_1);
+        contenido.appendChild(tabla);
     }
     else {
         alert("la extensión no se puede cargar :(");
--- typescript/content.ts
+++ typescript/content.ts
@@ -1,5 +1,19 @@
 var table = document.querySelector<HTMLTableElement>("#pageContent > div.datatable > div:nth-child(6) > table");
 
+function delay(ms: number){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function get_last_contest(handle: string, ms: number){
+    delay(ms);
+    let url = `https://codeforces.com/api/user.rating?handle=${handle}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(handle, data.status);
+    return data;
+}
+
+
 if (table){
 
     const names: string[] = [];
@@ -17,6 +31,7 @@ if (table){
             const tr = document.createElement("tr");
             tr.innerHTML = `<td class="dark left">${index}</td>\n<td style="text-align:left;" class="dark">${name}</td>\n<td class="dark right">last</td>`;
             tabla.appendChild(tr);
+            get_last_contest(name, index*1000);
         });
         contenido.appendChild(tabla);
     }else{
