3399dac9f6942f8c7f155f2dc27f5331565f195f
unknown <jhamsidrbz@gmail.com>
2025-03-18 12:08:40 -0500

    I Added memory

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,4 +1,5 @@
 "use strict";
+// ^ iniciamos los datos
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,30 +9,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+let reset = false;
 var table = document.querySelector("#pageContent > div.datatable > div:nth-child(6) > table");
 function init() {
-    // obtener ID
-    let id;
-    // TODO verificar que exista datos almacenados
-    chrome.storage.local.get(["id"]).then((result) => {
-        if (result.id == undefined) {
-            console.log("No se encuentra id, se creará uno por defecto");
-            chrome.storage.local.set({ "id": -1 });
-            id = -1;
+    return __awaiter(this, void 0, void 0, function* () {
+        // verificar el último contest 
+        const url = "https://codeforces.com/api/contest.list";
+        const response = yield fetch(url);
+        const data = yield response.json();
+        // verifica la data está disponible
+        try {
+            // ? verificamos si se obtuvo correctamente los datos
+            if (data.status != "OK") {
+                // ! Caso que no exista, mandamos error
+                throw new Error("Could not be obtained the list contest");
+            }
+            else {
+                // ? verificamos primeramente si existe la variable id
+                chrome.storage.local.get(["id"]).then((result) => {
+                    if (result.id == undefined) {
+                        // & caso que no exista, debemos crear uno
+                        chrome.storage.local.set({ "id": -1 });
+                    }
+                    else {
+                        // ? caso que exista, verificamos si el elemento es el actualizado
+                        if (result.id != data.result[-1].id) {
+                            // & Si son diferentes, entonces actualizamos y mandamos a resetear todo
+                            reset = true;
+                        }
+                    }
+                });
+            }
         }
-        else {
-            id = result.id;
-            console.log(`Id ${id}`);
+        catch (error) {
+            // ! mandamos el error
+            console.error(`Error: ${error}`);
         }
     });
-    // obtener dato del último contest
 }
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// Devuelve el estado
 function get_last_contest(handle, ms) {
     return __awaiter(this, void 0, void 0, function* () {
-        delay(ms);
+        //delay(ms);
         let url = `https://codeforces.com/api/user.rating?handle=${handle}`;
         const response = yield fetch(url);
         const data = yield response.json();
@@ -39,14 +58,41 @@ function get_last_contest(handle, ms) {
         return data;
     });
 }
-init();
+// * Acá sacamos los nombres de los amigos de los usuarios
 if (table) {
     const names = [];
     const fila = table.rows;
     for (let i = 1; i < fila.length; i++) {
         //console.log(fila[i].innerText.split('\t')[1]);
+        // & Agregamos los nombres en un arreglo
         names.push(fila[i].innerText.split('\t')[1]);
     }
+    /*
+        * Antes de insertar, debo asegurar que los elementos estén
+        & caso no exista, debo crearlo, y en caso sí existe, debo poner el valor de reset
+    */
+    names.forEach((name, index) => {
+        // ? verificamos que existe elemento
+        chrome.storage.local.get([name]).then((result) => {
+            if (result[name]) {
+                //? Caso que exista, verificar si se debe hacer un reseteo general
+                if (reset == true) {
+                    // & caso verdadero, pues, se reseta todo
+                    let reseteo = result[name];
+                    reseteo.set("reset", true);
+                    chrome.storage.local.set({ [name]: reseteo });
+                }
+            }
+            else {
+                // & caso exista, crearlo
+                chrome.storage.local.set({ [name]: {
+                        "reset": false,
+                        "contestname": "void", // TODO Esto lo hago después creo
+                        "rating": 0
+                    } });
+            }
+        });
+    });
     const contenido = document.querySelector("#pageContent");
     if (contenido) {
         const tabla = document.createElement("table");
@@ -54,7 +100,6 @@ if (table) {
             const tr = document.createElement("tr");
             tr.innerHTML = `<td class="dark left">${index}</td>\n<td style="text-align:left;" class="dark">${name}</td>\n<td class="dark right">last</td>`;
             tabla.appendChild(tr);
-            //get_last_contest(name, index*1000); //FIXME que no sejecute al mismo tiempo
         });
         contenido.appendChild(tabla);
     }
--- typescript/content.ts
+++ typescript/content.ts
@@ -1,32 +1,45 @@
+// ^ iniciamos los datos
+
+let reset = false;
 var table = document.querySelector<HTMLTableElement>("#pageContent > div.datatable > div:nth-child(6) > table");
 
-function init(){
-    // obtener ID
-    let id: number;
-    // TODO verificar que exista datos almacenados
-    chrome.storage.local.get(["id"]).then((result) => {
-        if (result.id == undefined){
-            console.log("No se encuentra id, se creará uno por defecto");
-            chrome.storage.local.set({"id": -1});
-            id = -1;
+
+
+async function init() {
+    // verificar el último contest 
+    const url = "https://codeforces.com/api/contest.list";
+    const response = await fetch(url);
+    const data = await response.json();
+    // verifica la data está disponible
+    try{
+        // ? verificamos si se obtuvo correctamente los datos
+        if (data.status != "OK"){
+            // ! Caso que no exista, mandamos error
+            throw new Error("Could not be obtained the list contest");
         }else{
-            id = result.id;
-            console.log(`Id ${id}`);
+            // ? verificamos primeramente si existe la variable id
+        chrome.storage.local.get(["id"]).then((result) => {
+            if (result.id == undefined){
+                // & caso que no exista, debemos crear uno
+                chrome.storage.local.set({"id": -1});
+            }else{
+                // ? caso que exista, verificamos si el elemento es el actualizado
+                if (result.id != data.result[-1].id){
+                    // & Si son diferentes, entonces actualizamos y mandamos a resetear todo
+                    reset = true;
+                } 
+            }
+        });
         }
-    });
-
-    // obtener dato del último contest
-    
-    
-
+    }catch (error){
+        // ! mandamos el error
+        console.error(`Error: ${error}`);
+    }
 }
 
-function delay(ms: number){
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
+// Devuelve el estado
 async function get_last_contest(handle: string, ms: number){
-    delay(ms);
+    //delay(ms);
     let url = `https://codeforces.com/api/user.rating?handle=${handle}`;
     const response = await fetch(url);
     const data = await response.json();
@@ -34,16 +47,45 @@ async function get_last_contest(handle: string, ms: number){
     return data;
 }
 
-init();
 
+// * Acá sacamos los nombres de los amigos de los usuarios
 if (table){
 
     const names: string[] = [];
     const fila = table.rows;
     for (let i: number = 1; i < fila.length; i++){
         //console.log(fila[i].innerText.split('\t')[1]);
+        // & Agregamos los nombres en un arreglo
         names.push(fila[i].innerText.split('\t')[1]);
     }
+
+    /*
+        * Antes de insertar, debo asegurar que los elementos estén
+        & caso no exista, debo crearlo, y en caso sí existe, debo poner el valor de reset
+    */
+
+    names.forEach((name: string, index: number) => {
+        // ? verificamos que existe elemento
+        chrome.storage.local.get([name]).then((result) => {
+            if (result[name]){
+                //? Caso que exista, verificar si se debe hacer un reseteo general
+                if (reset == true){
+                    // & caso verdadero, pues, se reseta todo
+                    let reseteo = result[name];
+                    reseteo.set("reset", true);
+                    chrome.storage.local.set({[name]: reseteo});
+                }
+
+            }else{
+                // & caso exista, crearlo
+                chrome.storage.local.set({[name]: {
+                    "reset": false,
+                    "contestname": "void", // TODO Esto lo hago después creo
+                    "rating": 0
+                }});
+            }
+        });
+    });
 
     const contenido = document.querySelector<HTMLDivElement>("#pageContent");
     if (contenido){
@@ -53,8 +95,7 @@ if (table){
             const tr = document.createElement("tr");
             tr.innerHTML = `<td class="dark left">${index}</td>\n<td style="text-align:left;" class="dark">${name}</td>\n<td class="dark right">last</td>`;
             tabla.appendChild(tr);
-            //get_last_contest(name, index*1000); //FIXME que no sejecute al mismo tiempo
-
+           
         });
         contenido.appendChild(tabla);
     }else{
